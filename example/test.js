851c93d28edbe678c536de621870e78023445ae0
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-05-09 17:53:15 +0200

    Fix #1

--- example/test.js
+++ example/test.js
@@ -4,7 +4,7 @@ var {JSDOM} = require('jsdom');
 var RDFaParser = require('../src/index.js');
 
 // Picked example from RDFa play
-var html = '<div vocab="http://schema.org/" typeof="Person"><a property="image" href="http://manu.sporny.org/images/manu.png">    <span property="name">Manu Sporny</span></a>,   <span property="jobTitle">Founder/CEO</span>  <div>    Phone: <span property="telephone">(540) 961-4469</span>  </div>  <div>    E-mail: <a property="email" href="mailto:msporny@digitalbazaar.com">msporny@digitalbazaar.com</a> </div>  <div>    Links: <a property="url" href="http://manu.sporny.org/">Manu’s homepage</a>  </div></div>';
+var html = '<div typeof="foaf:Person"><a property="foaf:depiction" href="http://manu.sporny.org/images/manu.png">    <span property="rdfs:label">Manu Sporny</span></a></div>';
 
 var doc = (new JSDOM(html, { url: "https://example.org/", contentType: "text/html"})).window.document;
 
